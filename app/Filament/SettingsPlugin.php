c15d33dbc03c29a7d862a0b1d86720319c2947a6
roble <renanroble@gmail.com>
2025-12-13 12:45:41 +0000

    Refactor SettingsPlugin: update namespace and use correct ModulesFilamentPlugin trait

--- app/Filament/SettingsPlugin.php
+++ app/Filament/SettingsPlugin.php
@@ -2,13 +2,13 @@
 
 namespace Modules\Settings\Filament;
 
-use Coolsam\Modules\Concerns\ModuleFilamentPlugin;
+use App\Filament\ModulesFilamentPlugin;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
 
 class SettingsPlugin implements Plugin
 {
-    use ModuleFilamentPlugin;
+    use ModulesFilamentPlugin;
 
     public function getModuleName(): string
     {
